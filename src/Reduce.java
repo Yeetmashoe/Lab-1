c9a0b4a5bdf4cc4eb2b3807f14d2940d6b072169
Matthew <matthewchen@Matthews-MacBook-Air-106.local>
2025-05-08 19:31:18 -0400

    completed reduce.java

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static void main(String[] args) {
+        int count = 1;
+        int start = 100;
+
+        while (start > 1){
+            if(start % 2 == 0){
+                start = start / 2;
+            } else {
+                start = start - 1;
+            }
+            count++;
+        }
+        System.out.println(count);
+    }
 }
